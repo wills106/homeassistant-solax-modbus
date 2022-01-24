c55758eae6bad4b291bcc06453f24b727059ec7a
infradom <jan@Jans-MacBook-Air-2.fritz.box>
2022-01-24 17:00:22 +0100

    support Gen4 and serial modbus

--- solax_modbus/__init__.py
+++ solax_modbus/__init__.py
@@ -20,8 +20,8 @@ from .const import (
     DEFAULT_NAME,
     DEFAULT_SCAN_INTERVAL,
     DOMAIN,
-    DEFAULT_SERIAL,
-    DEFAULT_SERIAL_PORT,
+    CONF_SERIAL,
+    CONF_SERIAL_PORT,
     CONF_READ_GEN2X1,
     CONF_READ_GEN3X1,
     CONF_READ_GEN3X3,
@@ -36,6 +36,8 @@ from .const import (
     DEFAULT_READ_GEN4X3,
     DEFAULT_READ_X1_EPS,
     DEFAULT_READ_X3_EPS,
+    DEFAULT_SERIAL,
+    DEFAULT_SERIAL_PORT,
 )
 
 _LOGGER = logging.getLogger(__name__)
