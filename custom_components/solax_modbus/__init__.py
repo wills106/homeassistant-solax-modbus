4c776aa88dba2576835e2f203a399b1f9d7ab7c2
wills106 <william.f.swann@googlemail.com>
2022-07-25 11:33:00 +0100

    X3 Mic Test 4

--- custom_components/solax_modbus/__init__.py
+++ custom_components/solax_modbus/__init__.py
@@ -86,8 +86,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     # read serial number
     seriesnumber = 'unknown'
-    inverter_data = hub.read_holding_registers(unit=hub._modbus_addr, address=0x300, count=7)
-    if inverter_data.isError():   _LOGGER.error("cannot perform initial read for serial number")
+    inverter_data = hub.read_holding_registers(unit=hub._modbus_addr, address=0x0, count=7)
+    if inverter_data.isError():   inverter_data = hub.read_holding_registers(unit=hub._modbus_addr, address=0x300, count=7)
+    elif inverter_data.isError():   _LOGGER.error("cannot perform initial read for serial number")
     else: 
         decoder = BinaryPayloadDecoder.fromRegisters( inverter_data.registers, byteorder=Endian.Big )
         seriesnumber = decoder.decode_string(14).decode("ascii")
