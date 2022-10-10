11cc703363aee0b71a0545e572dbb924010faf9f
Jan C <infradom@belgadom.org>
2022-10-10 12:15:33 +0200

    verified all input registers on gen4

--- custom_components/solax_modbus/sensor.py
+++ custom_components/solax_modbus/sensor.py
@@ -10,7 +10,7 @@ import homeassistant.util.dt as dt_util
 
 from .const import ATTR_MANUFACTURER, DOMAIN, SENSOR_TYPES # GEN3_X1_SENSOR_TYPES, GEN3_X3_SENSOR_TYPES, GEN4_SENSOR_TYPES, GEN4_X1_SENSOR_TYPES, GEN4_X3_SENSOR_TYPES
 #from .const import X1_EPS_SENSOR_TYPES, X3_EPS_SENSOR_TYPES, GEN4_X1_EPS_SENSOR_TYPES, GEN4_X3_EPS_SENSOR_TYPES, SolaXModbusSensorEntityDescription
-from .const import REG_INPUT, REG_HOLDING, REGISTER_U32, REGISTER_S32
+from .const import REG_INPUT, REG_HOLDING, REGISTER_U32, REGISTER_S32, REGISTER_ULSB16MSB16
 from .const import matchInverterWithMask, SolaXModbusSensorEntityDescription
 
 
@@ -34,7 +34,7 @@ def splitInBlocks( descriptions ):
     blocks = []
     curblockregs = []
     for reg in descriptions:
-        if descriptions[reg].newblock or ((reg - start) > 120): 
+        if descriptions[reg].newblock or ((reg - start) > 100): 
             if ((end - start) > 0): 
                 newblock = block(start = start, end = end, order16 = descriptions[start].order16, order32 = descriptions[start].order32, descriptions = descriptions, regs = curblockregs)
                 blocks.append(newblock)
@@ -44,7 +44,7 @@ def splitInBlocks( descriptions ):
             else: _LOGGER.warning(f"newblock declaration found for empty block")
         else: 
             if start == INVALID_START: start = reg
-            if descriptions[reg].unit in (REGISTER_S32, REGISTER_U32):  end = reg + 2
+            if descriptions[reg].unit in (REGISTER_S32, REGISTER_U32, REGISTER_ULSB16MSB16):  end = reg + 2
             else: end = reg + 1
             curblockregs.append(reg)
     if ((end-start)>0): # close last block
