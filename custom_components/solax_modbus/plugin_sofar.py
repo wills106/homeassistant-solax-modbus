7752fc85f40607f9d9f0f124ecf7348431ada029
wills106 <william.f.swann@googlemail.com>
2022-11-16 14:09:16 +0000

    Updated plugin_sofar.py
    Added Inverter naming to serial detection.

--- custom_components/solax_modbus/plugin_sofar.py
+++ custom_components/solax_modbus/plugin_sofar.py
@@ -93,9 +93,9 @@ def determineInverterType(hub, configdict):
         seriesnumber = "unknown"
 
     # derive invertertype from seriiesnumber
-    if   seriesnumber.startswith('SP1ES120N6'):  invertertype = HYBRID | X3 # New Hybrid 20kW no PV
-    elif seriesnumber.startswith('SP1'):  invertertype = HYBRID | X3 | GEN # Older Might be single
-    elif seriesnumber.startswith('ZM1E'):  invertertype = HYBRID | X3 | GEN # Not actuall X3, needs changing
+    if   seriesnumber.startswith('SP1ES120N6'):  invertertype = HYBRID | X3 # HYD20KTL-3P no PV
+    elif seriesnumber.startswith('SP1'):  invertertype = HYBRID | X3 | GEN # HYDxxKTL-3P
+    elif seriesnumber.startswith('ZM1E'):  invertertype = HYBRID | X3 | GEN # HYDxxxxES, Not actually X3, needs changing
     elif seriesnumber.startswith('SA1'):  invertertype = PV | X1 # Older Might be single
     elif seriesnumber.startswith('SB1'):  invertertype = PV | X1 # Older Might be single
     elif seriesnumber.startswith('SC1'):  invertertype = PV | X3 # Older Probably 3phase
