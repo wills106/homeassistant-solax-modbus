95e9746189d58154c68baafa8eee5f03d1d54b69
Jan C <infradom@belgadom.org>
2023-01-23 09:49:35 +0100

    correct feedin_priority remotecontrol option

--- custom_components/solax_modbus/plugin_solax.py
+++ custom_components/solax_modbus/plugin_solax.py
@@ -136,7 +136,8 @@ def value_function_remotecontrol_recompute(initval, descr, datadict):
         ap_target = target - pv # subtract house load and pv
         power_control = "Enabled Power Control"
     elif power_control == "Enabled Feedin Priority": # alternative computation for Power Control
-        ap_target = 0 - houseload_nett - pv # subtract house load and pv
+        if pv > houseload_nett:  ap_target = 0 - houseload_nett - pv # subtract house load and pv
+        else:                    ap_target = 0 - houseload_nett
         power_control = "Enabled Power Control"
     elif power_control == "Disabled": 
         ap_target = target
