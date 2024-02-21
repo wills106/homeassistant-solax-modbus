310cf2b1b4e4acfb9fa71a07e160595bbc1feaf1
Zwer2k <jr.github.com@topofweb.de>
2024-02-21 19:45:14 +0000

    improve client connect and close handling

--- custom_components/solax_modbus/number.py
+++ custom_components/solax_modbus/number.py
@@ -82,7 +82,7 @@ class SolaXModbusNumber(NumberEntity):
         self._hub.async_add_solax_modbus_sensor(self._modbus_data_updated)
 
     async def async_will_remove_from_hass(self) -> None:
-        self._hub.async_remove_solax_modbus_sensor(self._modbus_data_updated)
+        await self._hub.async_remove_solax_modbus_sensor(self._modbus_data_updated)
 
     """ remove duplicate declaration
     async def async_set_value(self, native_value: float) -> None:
