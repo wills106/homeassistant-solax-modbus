ebc63a03241cc130b4dd4c274d373159c767abe4
Peter <peter.turcan@tekl.com>
2024-04-02 13:47:23 +0200

    selecting baud rate refactor

--- custom_components/solax_modbus/plugin_growatt.py
+++ custom_components/solax_modbus/plugin_growatt.py
@@ -248,6 +248,18 @@ SELECT_TYPES = [
     #
     ###
     GrowattModbusSelectEntityDescription(
+        name = "Select baud rate",
+        key = "Select_baud_rate",
+        register = 22,
+        option_dict = {
+                0: "9600bps",
+                1: "38400bps",
+            },
+        allowedtypes = GEN2 | GEN3 | GEN4 | GEN | X3,
+        entity_category = EntityCategory.CONFIG,
+        icon = "mdi:dip-switch",
+    ),
+    GrowattModbusSelectEntityDescription(
         name = "Limit Grid Export",
         key = "limit_grid_export",
         register = 122,
@@ -901,24 +913,7 @@ SELECT_TYPES = [
         allowedtypes = SPF,
         entity_category = EntityCategory.CONFIG,
         icon = "mdi:dip-switch",
-    ),
-    ###
-    #
-    # SPF Selects
-    #
-    ###
-    GrowattModbusSelectEntityDescription(
-        name = "Select baud rate",
-        key = "Select_baud_rate",
-        register = 22,
-        option_dict = {
-                0: "9600bps",
-                1: "38400bps",
-            },
-        allowedtypes = GEN2 | GEN3 | GEN4 | GEN | X3,
-        entity_category = EntityCategory.CONFIG,
-        icon = "mdi:dip-switch",
-    ),
+    ),    
 ]
 
 # ================================= Sennsor Declarations ============================================================
