d8452fa33fb4929361efa8c4f831bd4418f94a7f
TonyMathiesen <95135900+TonyMathiesen@users.noreply.github.com>
2024-09-25 18:13:05 +0200

    Update __init__.py

--- custom_components/solax_modbus/__init__.py
+++ custom_components/solax_modbus/__init__.py
@@ -599,7 +599,11 @@ class SolaXModbusHub:
             byteorder=self.plugin.order16, wordorder=self.plugin.order32
         )
         builder.reset()
-        builder.add_16bit_int(payload)
+        if payload <=  32767:
+            builder.add_16bit_int(payload)
+        else:
+            builder.add_16bit_uint(payload)  # Use unsigned 16-bit integer method for larger values
+        
         payload = builder.to_registers()
         async with self._lock:
             await self._check_connection()
