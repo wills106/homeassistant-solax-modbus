99840e2135444b3a8b14f881ed176fea643940ec
rosenrot00 <121129193+rosenrot00@users.noreply.github.com>
2025-09-03 11:22:10 +0200

    Update plugin_growatt.py

--- custom_components/solax_modbus/plugin_growatt.py
+++ custom_components/solax_modbus/plugin_growatt.py
@@ -89,8 +89,8 @@ class GrowattModbusSelectEntityDescription(BaseModbusSelectEntityDescription):
 class GrowattModbusSensorEntityDescription(BaseModbusSensorEntityDescription):
     """A class that describes Growatt Modbus sensor entities."""
     allowedtypes: int = ALLDEFAULT # maybe 0x0000 (nothing) is a better default choice
-    order16: int = Endian.BIG
-    order32: int = Endian.BIG
+    order16: str = "big"
+    order32: str = "big"
     unit: int = REGISTER_U16
     register_type: int = REG_HOLDING
 
@@ -9414,7 +9414,7 @@ plugin_instance = growatt_plugin(
     SELECT_TYPES = SELECT_TYPES,
     SWITCH_TYPES = [],
     block_size = 100,
-    #order16 = Endian.BIG,
+    #order16 = "big",
     order32 = "big",
     auto_block_ignore_readerror = True
     )
