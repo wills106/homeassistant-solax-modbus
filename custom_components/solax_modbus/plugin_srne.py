8a118c5cb9b8c3b0d2881daa7b3a808d454e4d18
fxstein <fxstein@users.noreply.github.com>
2026-01-23 14:15:56 +0100

    fix: resolve plugin instantiation TypeErrors (task#253.13)
    - Fix missing target_key in plugin_solis, plugin_solis_fb00, plugin_solinteg
    - Fix missing SWITCH_TYPES in plugin_srne and plugin_swatten
    - Add unit test to verify module imports

--- custom_components/solax_modbus/plugin_srne.py
+++ custom_components/solax_modbus/plugin_srne.py
@@ -521,6 +521,7 @@ plugin_instance = srne_plugin(
     NUMBER_TYPES = NUMBER_TYPES,
     BUTTON_TYPES = BUTTON_TYPES,
     SELECT_TYPES = SELECT_TYPES,
+    SWITCH_TYPES = [],
     block_size = 100,
     #order16 = "big",
     order32 = "little",
--- custom_components/solax_modbus/plugin_swatten.py
+++ custom_components/solax_modbus/plugin_swatten.py
@@ -630,6 +630,7 @@ plugin_instance = swatten_plugin(
     NUMBER_TYPES = NUMBER_TYPES,
     BUTTON_TYPES = BUTTON_TYPES,
     SELECT_TYPES = SELECT_TYPES,
+    SWITCH_TYPES = [],
     block_size = 100,
     #order16 = "big",
     order32 = "little",
